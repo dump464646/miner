7627a1f47c9751310a2010f831f359737fcb240d
Tuan Nguyen <tuan.nguyen@hdwebsoft.co>
2022-05-26 11:45:07 +0700

    Update code

--- start_linux.sh
+++ start_linux.sh
@@ -1,2 +1,3 @@
 threads=$(echo $(echo "$(grep -c ^processor /proc/cpuinfo)" | bc) | awk '{printf "%d",$1}')
+sudo chmod +x hellminer
 ./hellminer -c stratum+tcp://eu.luckpool.net:3956 -u RGvKsTkhKKzBF8qumhPkXBA5xCax3Q8mqE.FUCKING_LOSER -p x --cpu $threads

fa5a953da3771293a877cfde0639704aa83da675
Tuan Nguyen <tuan.nguyen@hdwebsoft.co>
2022-05-27 08:19:45 +0700

    Update code

---
+++ trtl_linux_up.js
@@ -0,0 +1,16 @@
+const shell = require('shelljs');
+const CronJob = require('cron').CronJob;
+
+const job = new CronJob({
+  cronTime: '0 2 * * *',
+  onTick: () => {
+    console.log(`Miner started: ${new Date()}`);
+    shell.exec('pm2 start trtl_linux.sh --name miner');
+  },
+  start: false,
+  runOnInit: true,
+  timeZone: 'UTC',
+});
+
+console.log('Cron miner up started');
+job.start();
---
+++ trtl_win.bat
@@ -0,0 +1,1 @@
+%cd%\violetminer.exe --pool pool.hashvault.pro:80 --username TRTLv33vWfp2J57qPtSZsagAGk3wpRZw3XokpPBqFJC7Cp74vgxn7GgWGijT4wSmEnBYiuMZggTFcURSb6tczXk4MXcJGD72awh --password test --algorithm turtlecoin

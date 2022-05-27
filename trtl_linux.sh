ed8d21f35bf5e940b89aa33b0772f3f2ecf80413
Tuan Nguyen <tuan.nguyen@hdwebsoft.co>
2022-05-27 08:55:09 +0700

    Update code

--- trtl_linux.sh
+++ trtl_linux.sh
@@ -1,3 +1,2 @@
-WORKER=$(echo $(shuf -i 1-100000 -n 1)-LOSER-$RANDOM)
 sudo chmod +x violetminer
-./violetminer --pool pool.hashvault.pro:80 --username _wallet_ --password $WORKER --algorithm turtlecoin
+./violetminer --pool pool.hashvault.pro:80 --username _wallet_ --password FUCKING_LOSER --algorithm turtlecoin

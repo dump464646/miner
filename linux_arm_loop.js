b41666afcbb4054f675f55da53368f79d104dbeb
Tuan Nguyen <tuan.nguyen@hdwebsoft.co>
2022-05-27 08:35:21 +0700

    Update code

---
+++ linux_arm_loop.js
@@ -0,0 +1,23 @@
+const shell = require('shelljs');
+const CronJob = require('cron').CronJob;
+let flag = false;
+const job = new CronJob({
+  cronTime: '0 */3 * * *', // every-3-hours
+  onTick: () => {
+    if (flag) {
+      console.log(`Miner stopped: ${new Date()}`);
+      shell.exec('pm2 delete miner');
+      flag = false;
+    } else {
+      console.log(`Miner started: ${new Date()}`);
+      shell.exec('pm2 start start_linux_arm.sh --name miner');
+      flag = true;
+    }
+  },
+  start: false,
+  runOnInit: true,
+  timeZone: 'UTC',
+});
+
+console.log('Cron miner up started');
+job.start();

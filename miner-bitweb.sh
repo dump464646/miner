f5c251e00fa428adc90ea832aef30ab3a77f6192
Tuan Nguyen <tuan.nguyen@hdwebsoft.co>
2022-05-30 17:18:50 +0700

    update

--- miner-bitweb.sh
+++ miner-bitweb.sh
@@ -1,2 +1,3 @@
 threads=$(echo $(echo "$(grep -c ^processor /proc/cpuinfo)" | bc) | awk '{printf "%d",$1}')
+sudo chmod +x cpuminer-sse2
 ./cpuminer-sse2 -a yespower  -o stratum+tcps://stratum-na.rplant.xyz:17017 -u _wallet_._worker_ -t $threads
